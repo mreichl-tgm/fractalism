ff9905b9c7dd22c81e79652a2b6a91990fdf4965
Markus Reichl <re1@a1.net>
2016-05-11 13:55:04 +0200

    Improving UI a lot!

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
+var inputAngle = document.getElementById("angle");
+var inputSplit = document.getElementById("split");
+var inputDepth = document.getElementById("depth");
+var inputSize = document.getElementById("size");
+
+
 function rad(n) { return n * Math.PI / 180.0; }
 
 function drawLine(x1, y1, x2, y2){
@@ -10,9 +16,7 @@ function drawLine(x1, y1, x2, y2){
 
 function drawTree (angle, split, depth, size) {
     canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-
-    context.fillStyle = "#FF0";
+    canvas.height = document.body.clientHeight - 100;
 
     var x1 = canvas.width / 2;
     var y1 = 0;
@@ -31,6 +35,14 @@ function drawTree (angle, split, depth, size) {
 }
 
 function init() {
-    drawTree(90, 30, 20, 10);
+    var angle = parseInt(inputAngle.value);
+    var split = parseInt(inputSplit.value);
+    var depth = parseInt(inputDepth.value);
+    var size = parseInt(inputSize.value);
+
+    context.beginPath();
+    drawTree(angle, split, depth, size);
+    context.strokeStyle = "#fff";
     context.stroke();
+    context.closePath();
 }
