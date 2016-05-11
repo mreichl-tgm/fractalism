3b1c90afe824fe1918da138c9dc0c8f9e6e5ad42
Markus Reichl <re1@a1.net>
2016-05-11 10:24:25 +0200

    Moving js code to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+function rad(x) { return x * Math.PI / 180.0; }
+
+function drawLine(x1, y1, x2, y2){
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+}
+
+var drawTree = function (angle, split, depth, size) {
+    canvas.width = depth * angle;
+    canvas.height = depth * split * 2;
+
+    var x1 = canvas.width / 2;
+    var y1 = 0;
+
+    this.paint = function (x1, y1, angle, depth) {
+        if (depth != 0){
+            var x2 = x1 + (Math.cos(rad(angle)) * depth * size);
+            var y2 = y1 + (Math.sin(rad(angle)) * depth * size);
+            drawLine(x1, y1, x2, y2, depth);
+            this.paint(x2, y2, angle - split, depth - 1);
+            this.paint(x2, y2, angle + split, depth - 1);
+        }
+    }
+
+    this.paint(x1, y1, angle, depth);
+    context.stroke();
+}
+
+drawTree(90, 30, 10, 10);
