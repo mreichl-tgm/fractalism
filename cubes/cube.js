97bdb9b2c71a2c8409080365d07db20908a0b836
Markus Reichl <re1@a1.net>
2016-05-18 09:42:51 +0200

    Adding documentation to cube.js

--- cubes/cube.js
+++ cubes/cube.js
@@ -13,14 +13,23 @@
 var inputSize = document.getElementById("size");
 var inputSplit = document.getElementById("split");
 var inputDepth = document.getElementById("depth");
-
+/**
+ * Initializes cube drawing
+ */
 function drawCube() {
     var split = parseInt(inputSplit.value);
     var depth = parseInt(inputDepth.value);
     var size = parseInt(inputSize.value);
     
     var material = new THREE.MeshBasicMaterial( { wireframe: true } );
-
+    /**
+     * Draws a cube on each side of a given cube with a split amount of its size
+     * @param x
+     * @param y
+     * @param z
+     * @param size
+     * @param depth
+     */
     function paint(x, y, z, size, depth) {
         var cube = new THREE.Mesh( new THREE.BoxGeometry( size, size, size ), material );
         x = cube.position.x = x;
@@ -44,7 +53,9 @@ function drawCube() {
 
     paint(0, 0, 0, size, depth);
 }
-
+/**
+ * Fills input fields with working random values
+ */
 function setRandom() {
     inputSize.value = Math.floor((Math.random() * 10) + 1);
     inputSplit.value = Math.floor((Math.random() * 5) + 1);
